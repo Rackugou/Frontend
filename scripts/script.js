6a537359406eac6fca60650c2355f9aa5ac17287
Rackugou <152178326+Rackugou@users.noreply.github.com>
2024-09-18 19:15:49 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -6,8 +6,8 @@ let menuContent = document.querySelector("header > button:nth-of-type(1) img");
 menuButton.onclick = () => {
     menu.classList.toggle("toonMenu");
     if (menuContent.src.includes("/images/menu.png")){
-        menuContent.src = "/images/close_menu.png";
+        menuContent.src = "./images/close_menu.png";
 }   else {
-        menuContent.src = "/images/menu.png";
+        menuContent.src = "./images/menu.png";
 }   
 };
