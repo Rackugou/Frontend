e73e33adb804f04a89da0e12d6168fd6829db5b9
Rackugou <152178326+Rackugou@users.noreply.github.com>
2024-10-03 16:58:52 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -12,10 +12,10 @@ let bal2 = document.querySelector("main section:nth-of-type(1) > img:nth-of-type
 
 menuButton.onclick = () => {
     menu.classList.toggle("toonMenu");
-    if (menuContent.src.includes("./images/menu.png")){
-        menuContent.src = "./images/close_menu.png";
+    if (menuContent.src.includes("images/menu.png")){
+        menuContent.src = "../images/close_menu.png";
 }   else {
-        menuContent.src = "./images/menu.png";
+        menuContent.src = "../images/menu.png";
 }   
 };
 
@@ -25,31 +25,31 @@ buttonLinks.onclick = () => {
         
         
         if (card1.classList.contains("naarLinks")){
-                bal1.src = "./images/inactive_ball.png";
-                bal2.src = "./images/active_ball.png";
-                card2.removeAttribute("inert")
-                card1.setAttribute("inert", true)
+                bal1.src = "../images/inactive_ball.png";
+                bal2.src = "../images/active_ball.png";
+                card2.removeAttribute("inert");
+                card1.setAttribute("inert", true);
         } else{
-                card1.removeAttribute("inert")
-                card2.setAttribute("inert", true)
-                bal1.src = "./images/active_ball.png";
-                bal2.src = "./images/inactive_ball.png";
+                card1.removeAttribute("inert");
+                card2.setAttribute("inert", true);
+                bal1.src = "../images/active_ball.png";
+                bal2.src = "../images/inactive_ball.png";
         }
-}
+};
 
 buttonRechts.onclick = () => {
         card1.classList.toggle("naarLinks");
         card2.classList.toggle("naarLinks");
 
         if (card1.classList.contains("naarLinks")){
-                bal1.src = "./images/inactive_ball.png";
-                bal2.src = "./images/active_ball.png";
-                card2.removeAttribute("inert")
-                card1.setAttribute("inert", true)
+                bal1.src = "../images/inactive_ball.png";
+                bal2.src = "../images/active_ball.png";
+                card2.removeAttribute("inert");
+                card1.setAttribute("inert", true);
         } else{
-                card1.removeAttribute("inert")
-                card2.setAttribute("inert", true)
-                bal1.src = "./images/active_ball.png";
-                bal2.src = "./images/inactive_ball.png";
+                card1.removeAttribute("inert");
+                card2.setAttribute("inert", true);
+                bal1.src = "../images/active_ball.png";
+                bal2.src = "../images/inactive_ball.png";
         }
-}
+};
